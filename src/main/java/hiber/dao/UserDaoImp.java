17da0ff23a8d9d1f8e48769149834585e34cafce
andrejhodzinskij <hodzzinsky@yandex.ru>
2023-04-02 16:52:52 +0300

    Initial commit

---
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,46 @@
+package hiber.dao;
+
+import hiber.model.Car;
+import hiber.model.User;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+@EnableTransactionManagement
+public class UserDaoImp implements UserDao {
+
+   @Autowired
+   private SessionFactory sessionFactory;
+
+   @Override
+   public void add(User user) {
+      sessionFactory.getCurrentSession().save(user);
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public List<User> listUsers() {
+      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+      return query.getResultList();
+   }
+
+
+
+   @Override
+   @Transactional
+   public User getUserByCar(String model, int series) {
+      Car car =  sessionFactory.getCurrentSession()
+              .createQuery("select c from Car c where c.series=:series and c.model=:model", Car.class)
+              .setParameter("series", series)
+              .setParameter("model", model)
+              .getSingleResult();
+
+      return sessionFactory.getCurrentSession().get(User.class, car.getId());
+   }
+}
